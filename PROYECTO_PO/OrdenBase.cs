c70c561924438f3387d705362fa18dd0c8a4b672
TONYMOR1208 <106847101+TONYMOR1208@users.noreply.github.com>
2023-01-17 15:11:38 -0500

    Delete PROYECTO_PO directory

--- PROYECTO_PO/OrdenBase.cs
+++
@@ -1,7 +0,0 @@
-interface OrdenBase
-{
-    public double cuantificar();
-}
-    //Agregamos la clase llamada OrdenBase que trabaja
-    //como polimorfismo por interface, aqui
-    //integramos el metodo cuantificar 
